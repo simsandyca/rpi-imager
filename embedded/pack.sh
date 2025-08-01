4b7d63178b02038f3049e4c4bb0751e5c8d5b63e
Sandy Sim <sandy@www.hobosuit.com>
2025-08-01 12:09:35 -0700

    custom rpi-imager for my pi cluster

---
+++ embedded/pack.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+if [[ $UID != 0 ]]
+then
+   echo "you gotta be root for this script"
+   exit 1
+fi
+LOOP=$(losetup -f)
+echo "LOOP is $LOOP"
+if [[ ! -d boot ]]
+then
+   mkdir -p boot 
+fi
+if [[ ! -f boot.img ]]
+then
+   dd if=/dev/zero of=boot.img bs=1M count=32
+   losetup -P $LOOP boot.img
+   mkfs.vfat $LOOP
+else 
+   losetup -P $LOOP boot.img
+fi
+echo "mount $LOOP boot"
+mount $LOOP boot
+cd output 
+cp -rp * ../boot
+cd ..
+umount boot
+losetup -d $LOOP
+
+./rpi-eeprom-digest -k ../private.pem -i boot.img -o boot.sig 
+
